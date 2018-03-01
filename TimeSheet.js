d8a01b9ff9097a777f7273204ef5386392826525
Maldrek <scott.salem5@gmail.com>
2018-02-28 19:50:37 -0600

    added initial site files

---
+++ TimeSheet.js
@@ -0,0 +1,10 @@
+// Link to Firebase
+var config = {
+    apiKey: "AIzaSyCPsL6GxLYabuK6v0U8-RUk8nxglWeWusw",
+    authDomain: "maldrek-countdown-counter.firebaseapp.com",
+    databaseURL: "https://maldrek-countdown-counter.firebaseio.com",
+    projectId: "maldrek-countdown-counter",
+    storageBucket: "maldrek-countdown-counter.appspot.com",
+    messagingSenderId: "977661607836"
+  };
+  firebase.initializeApp(config);
